555a58ced1a798e44aff45e14b07107b5fb7f608
RustemYeldessov <eldesov1997@gmail.com>
2026-04-03 15:31:29 +0500

    refactor: Include auth route to main.py

--- src/main.py
+++ src/main.py
@@ -1,67 +1,13 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+from src.database import engine, Base
+from src.auth.router import router as auth_router
 
-app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
-users = [
-    {
-        "id": 1,
-        "first_name": "Rustem",
-        "age": 28,
-    },
-    {
-        "id": 2,
-        "first_name": "Ilmira",
-        "age": 28,
-    },
-]
+app = FastAPI(title="Qaltamda API")
 
-
-@app.get("/", summary="Стартовая страница")
-def main():
-    return "Qaltamda"
-
-
-@app.get(
-    "/users",
-    tags=["Пользователи"],
-    summary="Получить всех пользователей"
-)
-def get_users():
-    return users
-
-
-@app.get(
-    "/users/{user_id}",
-    tags=["Пользователи"],
-    summary="Получить конкретного пользователя"
-)
-def get_user(user_id: int):
-    for user in users:
-        if user["id"] == user_id:
-            return user
-    raise HTTPException(status_code=404, detail="Пользователь не найден")
-
-
-class NewUser(BaseModel):
-    first_name: str
-    age: int
-
-#Создание нового пользователя
-@app.post(
-    "/users",
-    tags=["Пользователи"],
-    summary="Добавить нового пользователя"
-)
-def create_user(new_user: NewUser):
-    users.append({
-        "id": len(users) + 1,
-        "first_name": new_user.first_name,
-        "age": new_user.age,
-    })
-    return {"success": True, "message": "Пользователь успешно создан"}
-
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
