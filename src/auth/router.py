1edd1c4762ac071579a17188f8b0b59622ac793a
RustemYeldessov <eldesov1997@gmail.com>
2026-04-06 15:39:45 +0500

    refactor: Add read_users method

--- src/auth/router.py
+++ src/auth/router.py
@@ -5,7 +5,22 @@ from src.auth import models, schemas, service
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
-@router.post("/register", response_model=schemas.UserOut)
+
+@router.get(
+    "/users",
+    response_model=list[schemas.UserOut],
+    summary="All users list"
+)
+def read_users(
+        skip: int = 0,
+        limit: int = 50,
+        db: Session = Depends(get_db)
+):
+    users = db.query(models.UserModel).offset(skip).limit(limit).all()
+
+    return users
+
+@router.post("/register", response_model=schemas.UserOut, status_code=201)
 def register_user(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
     # Проверяем, нет ли уже такого email
     user = db.query(models.UserModel).filter(models.UserModel.email == user_in.email).first()
@@ -14,9 +29,12 @@ def register_user(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="User already exists")
 
     hashed_pwd = service.hash_password(user_in.password)
+
     db_user = models.UserModel(
-        email=user_in.email,
         username=user_in.username,
+        email=user_in.email,
+        first_name=user_in.first_name,
+        last_name=user_in.last_name,
         hashed_password=hashed_pwd
     )
 
