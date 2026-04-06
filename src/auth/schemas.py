ed7b357957af795edaf27520e8414abd06985d07
RustemYeldessov <eldesov1997@gmail.com>
2026-04-06 15:40:23 +0500

    refactor: Update auth/schemas.py

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,15 +1,33 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, model_validator
 
+
+# Базовый класс, от него буду наследоваться UserCreate
+# и UserOut. Параметры этого класса будут переданы в оба класса-потомка
 class UserBase(BaseModel):
-    email: EmailStr
-
-class UserCreate(UserBase):
     username: str
-    password: str
+    email: EmailStr
+    first_name: str
+    last_name: str
 
+# Наследуется от базового класса. Здесь у создаваемого пользователя запрашивается
+# информация, которая не будет возвращена после его создания, но нужна для записи в БД
+class UserCreate(UserBase):
+    password: str
+    password_confirm: str
+
+    @model_validator(mode="after")
+    def check_passwords_match(self):
+        if self.password_confirm != self.password:
+            raise ValueError("Password do not match")
+        return self
+
+# Информация, которая вернется после создания пользователя помимо той, что
+# передается из базового класса UserBase
 class UserOut(UserBase):
     id: int
     is_active: bool
 
+    # from_attributes = True позволяет FastAPI работать
+    # не только со словарями, но и с объектами ORM
     class Config:
         from_attributes = True
